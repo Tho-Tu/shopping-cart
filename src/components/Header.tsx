4ce1ea7c351b9f3aa69268785a7d144a58825ddb
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2024-01-22 15:47:47 +1000

    Add search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput.tsx";
 
 function Header() {
   return (
-    <div className="flex justify-between py-4 px-8 bg-cyan-500">
-      <Link to="/" className="min-w-max text-white text-xl">
-        Open Beach
+    <div className="flex justify-between py-6 px-12 bg-cyan-500">
+      <Link to="/" className="min-w-max text-white text-2xl">
+        🏖️ OpenBeach
       </Link>
-      <nav className="flex gap-4 text-white">
-        <Link to="/home">Home</Link>
+      <div>
+        <SearchInput />
+      </div>
+      <nav className="flex gap-6 text-white text-lg">
+        <Link to="/sef">Error</Link>
         <Link to="/store">Store</Link>
         <Link to="/shopping-cart" className="min-w-max">
           Shopping Cart
