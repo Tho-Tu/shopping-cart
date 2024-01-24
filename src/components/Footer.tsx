0a16f0fd8ef863387c965bd3d4e27760367f159f
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2024-01-24 15:44:37 +1000

    Create footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+function Footer() {
+  return (
+    <div className="flex justify-end gap-2 py-4 px-12 bg-gray-700">
+      <div className="text-white">
+        Photo by{" "}
+        <a
+          className="text-blue-500"
+          href="https://unsplash.com/@joshtw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+        >
+          Josh Liu
+        </a>{" "}
+        on{" "}
+        <a
+          className="text-blue-500"
+          href="https://unsplash.com/photos/white-wall-with-paintings-on-wall-Tjio9DgtIls?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+        >
+          Unsplash
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
