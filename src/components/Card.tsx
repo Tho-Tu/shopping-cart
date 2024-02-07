24a26a84efb9f8f30e87c2d93d5081b7665f7dac
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2024-02-07 21:12:01 +1000

    Update item ui with store page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,9 +10,20 @@ export default function Card({ title, description, image, price }: cardProps) {
   return (
     <>
       <h3 className="font-bold">{title}</h3>
-      <div>{description}</div>
-      <img src={image}></img>
-      <div>{price}</div>
+      {/* <div className="text-sm">{description}</div> */}
+      <img
+        src={image}
+        alt={title}
+        draggable="false"
+        className="w-2/3 rounded-xl"
+      ></img>
+      <div className="font-bold">${price}</div>
+      <button
+        type="button"
+        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Add to cart
+      </button>
     </>
   );
 }
--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -19,7 +19,8 @@ const useCardData = () => {
     async function getData() {
       try {
         const response = await fetch(
-          "https://fakestoreapi.com/products?limit=15"
+          "https://fakestoreapi.com/products?limit=15",
+          { mode: "cors" }
         );
         if (!response.ok) {
           throw new Error(
@@ -56,11 +57,11 @@ function Store() {
     <div className="flex-1">
       <div className="flex flex-col gap-6 py-24 px-28">
         <h1>Fake Store API</h1>
-        {loading && <div>Item loading...</div>}
+        {loading && <div>Items loading...</div>}
         {error && (
           <div>{`There is a problem fetching the post data - ${error}`}</div>
         )}
-        <ul className="flex flex-wrap w-12">
+        <ul className="flex flex-wrap gap-4">
           {data &&
             (data as CardItem[]).map(
               (item: {
@@ -70,7 +71,10 @@ function Store() {
                 image: string;
                 price: number;
               }) => (
-                <li key={item.id} className="bg-gray-100 p-4">
+                <li
+                  key={item.id}
+                  className="border-4 border-solid border-r-indigo-500 border-b-indigo-500 p-4 w-80 flex flex-col justify-between rounded-xl"
+                >
                   <Card
                     title={item.title}
                     description={item.description}
