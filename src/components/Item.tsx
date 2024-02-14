7b97210138c2a07cb617028fe9388d7ab1478c26
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2024-02-14 18:30:21 +1000

    Update item ui

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useParams } from "react-router-dom";
+import { useParams, Form } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 type itemType = {
@@ -44,26 +44,49 @@ export default function Item() {
     getItem();
   }, [itemIndex]);
 
+  function capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
-    <div className="flex-1 flex flex-col gap-6 py-24 px-28">
-      {loading && <div>Item loading...</div>}
-      {error && (
-        <div>{`There is a problem fetching the post data - ${error}`}</div>
-      )}
-      <h3 className="font-bold">{item && item.title}</h3>
-      <div className="text-sm">{item && item.description}</div>
-      <img
-        src={item && item.image}
-        alt={item && item.title}
-        draggable="false"
-        className="w-40 rounded-xl self-center"
-      ></img>
-      <div className="font-bold">
-        ${item && item.price}
-        <br></br>
-        <button type="button" className="button-blue ">
-          Add to cart
-        </button>
+    <div className="flex-1 flex gap-8 py-24 px-28">
+      <div className="flex flex-col gap-10 w-3/5">
+        {loading && <div>Item loading...</div>}
+        {error && (
+          <div>{`There is a problem fetching the post data - ${error}`}</div>
+        )}
+        <h3 className="font-bold text-2xl">{item && item.title}</h3>
+        <div className="text-xl">
+          {item && capitalizeFirstLetter(item.description)}
+        </div>
+        {!loading && (
+          <div>
+            <div className="flex justify-between">
+              <div className="self-center text-xl">${item && item.price}</div>
+              <Form method="post" className="flex gap-3">
+                <input
+                  type="number"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 
+                text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                block w-full p-2.5"
+                  placeholder="0"
+                  required
+                ></input>
+                <button type="submit" className="button-blue min-w-fit">
+                  Add to cart
+                </button>
+              </Form>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="w-2/5">
+        <img
+          src={item && item.image}
+          alt={item && item.title}
+          draggable="false"
+          className="w-3/5 rounded-xl"
+        ></img>
       </div>
     </div>
   );
